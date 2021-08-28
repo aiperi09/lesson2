f88ff9fdb0686a63c0f2b35368b6c105124fa8b7
aiperi09 <aiperi.miran@bk.ru>
2021-08-28 20:35:21 +0300

    hghjf

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,10 +2,30 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args)
-        
-
-
+    public static void main(String[] args) {
+    weatherAge(45,15);
+    weatherAge(25,15);
+    weatherAge(15,35);
+    weatherAge(15,15);
+    weatherAge(15,95);
+    weatherAge(65,15);
 
     }
+
+    public static void weatherAge(int weather, int age ) {
+        if (age > 20 && age < 45 && weather > -20 && weather < 30) {
+            System.out.println("можно идти гулять");
+
+        } else if (age < 20 && weather > 0 && weather < 28) {
+            System.out.println("можно идти гулять");
+
+        } else if (age > 45 && weather > -10 && weather < 25) {
+            System.out.println("можно идти гулять");
+
+        } else {
+            System.out.println("оставайтесь дома");
+
+        }
+    }
 }
+
