98b1bca666099742c503cbd3cff2e35320dee4fd
aiperi09 <aiperi.miran@bk.ru>
2021-08-28 22:05:36 +0300

    nhhh

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,28 +3,28 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-    weatherAge(45,15);
-    weatherAge(25,15);
-    weatherAge(15,35);
-    weatherAge(15,15);
-    weatherAge(15,95);
-    weatherAge(65,15);
+    String method = "Можно идти гулять";
+    String method2 = "Оставайтесь дома";
+        System.out.println(weatherAge(40,25,method,method2));
+        System.out.println(weatherAge(100,50,method,method2));
+        System.out.println(weatherAge(15,35,method,method2));
+        System.out.println(weatherAge(15,15,method,method2));
+        System.out.println(weatherAge(15,95,method,method2));
+        System.out.println(weatherAge(65,15,method,method2));
+
+
 
     }
 
-    public static void weatherAge(int weather, int age ) {
+    public static String weatherAge(int weather, int age,String text,String text2) {
         if (age > 20 && age < 45 && weather > -20 && weather < 30) {
-            System.out.println("можно идти гулять");
-
+        return text;
         } else if (age < 20 && weather > 0 && weather < 28) {
-            System.out.println("можно идти гулять");
-
+        return text;
         } else if (age > 45 && weather > -10 && weather < 25) {
-            System.out.println("можно идти гулять");
-
+            return text;
         } else {
-            System.out.println("оставайтесь дома");
-
+            return text2;
         }
     }
 }
