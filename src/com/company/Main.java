f630b1c44b885946d27e85da7393b95df0a13891
aiperi09 <aiperi.miran@bk.ru>
2021-08-29 18:52:29 +0300

    nhhh

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,30 +1,50 @@
 package com.company;
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println(weatherAge(30,25));
-        System.out.println(weatherAge(100,50));
-        System.out.println(weatherAge(15,35));
-        System.out.println(weatherAge(15,15));
-        System.out.println(weatherAge(15,95));
-        System.out.println(weatherAge(65,15));
 
-
+        System.out.println(weatherAge(generateRandomAge(),15));
+        System.out.println(weatherAge(generateRandomAge(),13));
+        System.out.println(weatherAge(generateRandomAge(),25));
+        System.out.println(weatherAge(generateRandomAge(),45));
+        System.out.println(weatherAge(generateRandomAge(),25));
 
     }
+    public static String weatherAge(int age,int weather) {
 
-    public static String weatherAge(int weather, int age) {
-        if (age > 20 && age < 45 && weather >  - 20 && weather < 30) {
-        return "Можно идти гулять";
+        if (age > 20 && age < 45 && weather > -20 && weather < 30) {
+            return "можно идти гулять";
+
+
         } else if (age < 20 && weather > 0 && weather < 28) {
-            return "Можно идти гулять";
-        } else if (age > 45 && weather > -10 && weather < 25) {
-            return "Можно идти гулять";
-        } else {
-            return "Оставайтесь дома";
+            return "можно идти гулять";
+        }else if (age > 45 && weather >- 10 && weather < 25){
+            return "можно идти гулять";
+
+
+        }else {
+            return "оставайтесь дома";
+
         }
+
     }
+    public static int generateRandomAge(){
+
+        Random random = new Random();
+        int book = random.nextInt(50)+1;
+        return book;
+
+    }
+
+
+
 }
+
+
+
+
 
