dbef9e690f7a935829a4352c8bd58c71d8a8a463
aiperi09 <aiperi.miran@bk.ru>
2021-08-29 16:09:48 +0300

    nhhh

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,28 +3,27 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-    String method = "Можно идти гулять";
-    String method2 = "Оставайтесь дома";
-        System.out.println(weatherAge(40,25,method,method2));
-        System.out.println(weatherAge(100,50,method,method2));
-        System.out.println(weatherAge(15,35,method,method2));
-        System.out.println(weatherAge(15,15,method,method2));
-        System.out.println(weatherAge(15,95,method,method2));
-        System.out.println(weatherAge(65,15,method,method2));
+
+        System.out.println(weatherAge(30,25));
+        System.out.println(weatherAge(100,50));
+        System.out.println(weatherAge(15,35));
+        System.out.println(weatherAge(15,15));
+        System.out.println(weatherAge(15,95));
+        System.out.println(weatherAge(65,15));
 
 
 
     }
 
-    public static String weatherAge(int weather, int age,String text,String text2) {
-        if (age > 20 && age < 45 && weather > -20 && weather < 30) {
-        return text;
+    public static String weatherAge(int weather, int age) {
+        if (age > 20 && age < 45 && weather >  - 20 && weather < 30) {
+        return "Можно идти гулять";
         } else if (age < 20 && weather > 0 && weather < 28) {
-        return text;
+            return "Можно идти гулять";
         } else if (age > 45 && weather > -10 && weather < 25) {
-            return text;
+            return "Можно идти гулять";
         } else {
-            return text2;
+            return "Оставайтесь дома";
         }
     }
 }
